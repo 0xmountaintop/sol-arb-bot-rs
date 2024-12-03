fb3bd1fac3ae1e573cb415a8b535366b4702bc07
0xmountaintop <37070449+0xmountaintop@users.noreply.github.com>
2024-12-03 23:19:16 +1100

    feat: execute arbitrage (#1)

--- src/consts.rs
+++ src/consts.rs
@@ -1,5 +1,20 @@
 pub const WSOL_MINT: &str = "So11111111111111111111111111111111111111112";
 pub const USDC_MINT: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
-pub const RPC_URL: &str = "https://mainnet-ams.chainbuff.com";
-pub const JUP_V6_API_BASE_URL: &str = "http://127.0.0.1:8080";
 pub const JITO_TIP_ACCOUNT: &str = "Cw8CFyM9FkoMi7K7Crf6HNQqf4uEMzpKw6QNghXLvLkY";
+
+use lazy_static::lazy_static;
+use std::env;
+
+lazy_static! {
+    pub static ref RPC_URL: String = {
+        env::var("RPC_URL").unwrap_or_else(|_| "https://mainnet-ams.chainbuff.com".to_string())
+    };
+    pub static ref JUP_V6_API_BASE_URL: String = {
+        env::var("JUP_V6_API_BASE_URL")
+            .unwrap_or_else(|_| "http://127.0.0.1:8080".to_string())
+    };
+    pub static ref JITO_RPC_URL: String = {
+        env::var("JITO_RPC_URL")
+            .unwrap_or_else(|_| "https://frankfurt.mainnet.block-engine.jito.wtf/api/v1/bundles".to_string())
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
 use bot::ArbitrageBot;
+use env_logger::Builder;
+use log::LevelFilter;
+use std::io::Write;
 use std::time::Duration;
 
 mod bot;
@@ -8,11 +11,27 @@ mod types;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Initialize logger with timestamp
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    dotenv::dotenv().ok();
+
     let bot = ArbitrageBot::new()?;
-    
+
     loop {
         if let Err(e) = bot.run().await {
-            eprintln!("Error running bot: {}", e);
+            log::error!("Error running bot: {}", e);
         }
         tokio::time::sleep(Duration::from_millis(200)).await;
     }
